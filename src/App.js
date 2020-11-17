f82b8d4d26533659b2d1be4bc4eaf95362165df1
bonomoon <moonbonoz@gmail.com>
2020-11-17 22:27:02 +0900

    update: gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="background">
+        <div className="background-item"></div>
+      </div>
+      <div className="main">
+        <header className="header">
+          <p>
+            안녕안녕 내 이름은 봉오야~ 혜선이 안녕 준서 안녕
+          </p>
+        </header>
+        <main></main>
+        <footer></footer>
+      </div>
     </div>
   );
 }
 
-export default App;
