ce1849eb33dfa97a1ee5991794e93f5a0c265e99
seoyeon <67668501+SEOYEONGO@users.noreply.github.com>
2020-11-18 22:44:53 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="background">
+        <div className="background-item"></div>
+      </div>
+      <div className="main">
+        <header className="header">
+          <p>
+            안녕안녕 내 이름은 봉오야~ 혜선이 안녕 준서 안녕
+          </p>
+        </header>
+        <main></main>
+        <footer></footer>
+      </div>
+    </div>
+  );
+}
+
