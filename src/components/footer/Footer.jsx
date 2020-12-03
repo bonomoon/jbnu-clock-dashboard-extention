aacdfe15a9025ad98be8d88cf30f1549acfb273d
bonomoon <moonbonoz@gmail.com>
2020-12-03 10:28:57 +0900

    update: Header와 Footer 스타일 수정 - #10

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,22 @@
 // css import
-import './Footer.module.css';
+import styles from './Footer.module.css';
+
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 export default function Footer(props) {
   const { renderLeftComponent, renderCenterComponent, renderRightComponent } = props;
 
   return (
-    <header className={'app-footer'}>
-        <div className={'left-content'}>
+    <header className={cx('app-footer')}>
+        <div className={cx('left-content')}>
             {renderLeftComponent && renderLeftComponent()}
         </div>
-        <div className={'center-content'}>
+        <div className={cx('center-content')}>
             {renderCenterComponent && renderCenterComponent()}
         </div>
-        <div className={'right-content'}>
+        <div className={cx('right-content')}>
             {renderRightComponent && renderRightComponent()}
         </div>
     </header>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,22 @@
 // css import
-import './Header.module.css';
+import styles from './Header.module.css';
+
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 export default function Header(props) {
   const { renderLeftComponent, renderCenterComponent, renderRightComponent } = props;
 
   return (
-    <header className={'app-header'}>
-        <div className={'left-content'}>
+    <header className={cx('app-header')}>
+        <div className={cx('left-content')}>
             {renderLeftComponent && renderLeftComponent()}
         </div>
-        <div className={'center-content'}>
+        <div className={cx('center-content')}>
             {renderCenterComponent && renderCenterComponent()}
         </div>
-        <div className={'right-content'}>
+        <div className={cx('right-content')}>
             {renderRightComponent && renderRightComponent()}
         </div>
     </header>
