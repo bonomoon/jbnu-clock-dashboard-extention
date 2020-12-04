78ae35e84c07c98f116829ddac6faedae2a3bb11
bonomoon <moonbonoz@gmail.com>
2020-12-04 10:41:09 +0900

    update: weather component style

--- src/components/weather/Weather.jsx
+++ src/components/weather/Weather.jsx
@@ -16,49 +16,48 @@ const getWeatherApiURL = (lat, lon) => `https://api.openweathermap.org/data/2.5/
 const getWeatherIconURL = (icon) => `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
 export default function Weather() {
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState(null);
 
   const geo = useGeolocation();
 
   useEffect(() => {
-    if (
-      geo.position.latitude !== undefined &&
-      geo.position.longitude !== undefined
-    ) {
-      axios
-        .get(getWeatherApiURL(geo.position.latitude, geo.position.longitude))
-        .then((response) => {
-          setWeather({
-            cityName: response.data.name,
-            temperature: getCelsius(response.data.main.temp),
-            icon: response.data.weather[response.data.weather.length - 1].icon,
-          });
-        })
-        .catch(() => {
-          setWeather({
-            cityName: "Jeonju",
-            temperature: 0,
-            icon: "02d",
-          });
+    axios
+      .get(getWeatherApiURL(geo.position.latitude, geo.position.longitude))
+      .then((response) => {
+        setWeather({
+          cityName: response.data.name,
+          temperature: getCelsius(response.data.main.temp),
+          icon: response.data.weather[response.data.weather.length - 1].icon,
         });
-    }
+      })
+      .catch((error) => {
+        setWeather({
+          cityName: "Jeonju",
+          temperature: 0,
+          icon: "02d",
+        });
+      });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [geo.position]);
 
   return (
     <div className={cx("container")}>
-      <img
-        className={cx("weather-icon")}
-        src={getWeatherIconURL(weather.icon)}
-        alt={"weather-icon"}
-      ></img>
-      <div className={cx("weather-content")}>
-        <div>
-          <span className={cx("weather-temp")}>{weather.temperature}</span>
-          <span class={cx("weather-degree")}>°</span>
-        </div>
-        <span className={cx("weather-city-name")}>{weather.cityName}</span>
-      </div>
+      {weather !== null && (
+        <>
+          <img
+            className={cx("weather-icon")}
+            src={getWeatherIconURL(weather.icon)}
+            alt={"weather-icon"}
+          ></img>
+          <div className={cx("weather-content")}>
+            <div>
+              <span className={cx("weather-temp")}>{weather.temperature.toFixed(0)}</span>
+              <span className={cx("weather-degree")}>°</span>
+            </div>
+            <span className={cx("weather-city-name")}>{weather.cityName}</span>
+          </div>
+        </>
+      )}
     </div>
   );
 }
